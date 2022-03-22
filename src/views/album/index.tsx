cc4a07360cb93e8f9354ff3eb7f82e075d5d8add
cong <wangcong122@jd.com>
2022-03-22 22:26:15 +0800

    feat: album songs

--- src/views/album/index.tsx
+++ src/views/album/index.tsx
@@ -3,6 +3,22 @@ import { useParams } from 'react-router-dom'
 import { getAlbum } from '@/api'
 import Cover from '@/components/cover'
 
+function millis2MiniutesAndSeconds(millis: number) {
+  const minutes = Math.floor(millis / 60000);
+  const seconds = +((millis % 60000) / 1000).toFixed(0);
+  return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
+
+const AlbumSong = (props) => {
+  return (<div className="album-song flex items-center p-2 h-48px">
+    <div className="flex-1 font-semibold">
+      <span className="text-lg">{props.name}</span>
+      { props.alia.length > 0 && <span className="pl-2 text-gray-400">({props.alia})</span> }
+    </div>
+    <span>{millis2MiniutesAndSeconds(props.dt)}</span>
+  </div>)
+}
+
 const Album = () => {
   const { albumId } = useParams()
 
@@ -34,6 +50,12 @@ const Album = () => {
         <div>{album?.artist?.name}</div>
         <p className="text-sm text-gray-800 line-clamp-10 whitespace-pre-wrap">{album?.description}</p>
       </div>
+    </div>
+
+    <div className="album-songs">
+      {
+        songs.map(song => (<AlbumSong key={song.id} {...song} />))
+      }
     </div>
   </div>)
 }
