8fc513c08b83af2ec9dfd4785a980a73ccadc9f9
youngcong <1151651564@qq.com>
2022-05-28 19:23:17 +0800

    fix: modal style

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import * as React from 'react'
+import classNames from 'classnames'
 
 export interface ModalProps {
   open: boolean
@@ -26,18 +27,30 @@ const Modal = (props: ModalProps) => {
 
   return (
     <div className="modal" style={{ display: open ? 'block' : 'none' }}>
-      <div className="modal-overlay fixed inset-0 bg-white bg-opacity-58"/>
+      <div className={classNames(
+        'modal-overlay',
+        'fixed inset-0 bg-white bg-opacity-58')}
+      />
 
       <div
-        className="modal-content-wrap fixed inset-0 flex-center"
+        className={classNames(
+          'modal-content-wrap',
+          'fixed inset-0 z-1 flex-center',
+        )}
         onClick={onClickOverlay}
       >
         <div
-          className="modal-content bg-white bg-opacity-78 backdrop-filter backdrop-blur rounded-xl border shadow"
+          className={classNames(
+            'modal-content',
+            'bg-white bg-opacity-78 backdrop-filter backdrop-blur rounded-xl border shadow',
+          )}
           style={{ width }}
           onClick={(e) => { e.stopPropagation() }}
         >
-          <header className="modal-header flex items-center justify-between px-6 py-4 font-semibold">
+          <header className={classNames(
+            'modal-header',
+            'flex items-center justify-between px-6 py-4 font-semibold',
+          )}>
             <span className="text-xl">{title}</span>
             <i
               className="icon-close one-icon !text-xl !leading-none"
@@ -45,11 +58,17 @@ const Modal = (props: ModalProps) => {
             />
           </header>
 
-          <div className="modal-body px-6 py-2 whitespace-pre-wrap">
+          <div className={classNames(
+            'modal-body',
+            'max-h-[calc(100vh-236px)] overflow-auto px-6 py-2 whitespace-pre-wrap',
+          )}>
             {props.children || props.content}
           </div>
 
-          <div className="modal-footer px-6 py-4"></div>
+          <div className={classNames(
+            'modal-footer',
+            'px-6 py-4',
+          )} />
         </div>
       </div>
     </div>
